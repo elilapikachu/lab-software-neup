a99ee3bd808f5b48dbb317580fd76898201b19dc
Crazzys <elianaperezramos871@gmail.com>
2026-05-25 08:02:29 -0500

    Se empieza a conectar con las apis del back

---
+++ src/app/models/correo.ts
@@ -0,0 +1,5 @@
+export interface Correo {
+    to?: string;
+    subject?: string;
+    body?: string;
+}
---
+++ src/app/models/receta.ts
@@ -0,0 +1,46 @@
+export interface Ingrediente {
+  nombre_ingrediente: string;
+  cantidad: number;
+  tipo_ingrediente: string;
+}
+ 
+export interface Nutricion {
+  kcal: number;
+  proteinas: number;
+  carbohidratos: number;
+  fibra: number;
+  vitaminas: string[];
+  minerales: string[];
+}
+ 
+ 
+export interface RecetaResponse {
+  id: string;
+  nombre_receta: string;
+  ingredientes: Ingrediente[];
+  nutricion: Nutricion;
+  tags: string[];
+  tiempo_preparacion: string;
+  creada_por: string | null;
+  es_personalizada: boolean;
+  visibilidad: 'publica' | 'privada';
+  imagen: string[]; // lista de documentoIds
+}
+ 
+ 
+export interface RecetaRequest {
+  nombre_receta: string;
+  ingredientes: Ingrediente[];
+  nutricion: Nutricion;
+  tags: string[];
+  tiempo_preparacion: string;
+  creada_por: string | null;
+  es_personalizada: boolean;
+  visibilidad: 'publica' | 'privada';
+}
+ 
+ 
+export interface RecetaIdResponse {
+  id: string;
+  mensaje: string;
+}
